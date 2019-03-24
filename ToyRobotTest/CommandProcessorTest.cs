a885cfcbf5380b21aa4d08f2c8c6d4e695c7a87b
liyakuriakose <liyakuriakose@gmail.com>
2019-03-25 06:43:25 +1100

    Added few tests.

--- ToyRobotTest/CommandProcessorTest.cs
+++ ToyRobotTest/CommandProcessorTest.cs
@@ -42,7 +42,43 @@ namespace ToyRobotTest
         }
 
         [TestMethod]
-        public void CommandProcessor_ExecuteCommand()
+        public void CommandProcessor_Right_Fail()
+        {
+            CommandProcessor commandProcessor = new CommandProcessor();
+            commandProcessor.XMax = 5;
+            commandProcessor.YMax = 5;
+            commandProcessor.IsPlaced = true;
+            commandProcessor.FaceDirection = Direction.SOUTH;
+            commandProcessor.Right();
+            Assert.AreNotEqual(Direction.NORTH, commandProcessor.FaceDirection);
+        }
+
+        [TestMethod]
+        public void CommandProcessor_Left_Success()
+        {
+            CommandProcessor commandProcessor = new CommandProcessor();
+            commandProcessor.XMax = 5;
+            commandProcessor.YMax = 5;
+            commandProcessor.IsPlaced = true;
+            commandProcessor.FaceDirection = Direction.EAST;
+            commandProcessor.Right();
+            Assert.AreEqual(Direction.SOUTH, commandProcessor.FaceDirection);
+        }
+
+        [TestMethod]
+        public void CommandProcessor_Left_Fail()
+        {
+            CommandProcessor commandProcessor = new CommandProcessor();
+            commandProcessor.XMax = 5;
+            commandProcessor.YMax = 5;
+            commandProcessor.IsPlaced = true;
+            commandProcessor.FaceDirection = Direction.EAST;
+            commandProcessor.Right();
+            Assert.AreNotEqual(Direction.WEST, commandProcessor.FaceDirection);
+        }
+
+        [TestMethod]
+        public void CommandProcessor_ExecuteCommand_Success()
         {
             CommandProcessor commandProcessor = new CommandProcessor();
             commandProcessor.XMax = 5;
@@ -50,7 +86,43 @@ namespace ToyRobotTest
             commandProcessor.Commands = new string[] {"Place 0,1,North","Move","Right","Report"} ;
             commandProcessor.ExecuteCommand();
             Assert.AreEqual("0,2,EAST", commandProcessor.XPosRobot + "," + commandProcessor.YPosRobot + "," + commandProcessor.FaceDirection);
+        }
 
+        [TestMethod]
+        public void CommandProcessor_ExecuteCommand_InvalidCommand()
+        {
+            CommandProcessor commandProcessor = new CommandProcessor();
+            commandProcessor.XMax = 5;
+            commandProcessor.YMax = 5;
+            commandProcessor.Commands = new string[] { "Rotate 0,1,North", "Move", "Right", "Report" };
+            commandProcessor.ExecuteCommand();
+            Assert.AreEqual(false, commandProcessor.IsPlaced);
+        }
+
+        [TestMethod]
+        public void CommandProcessor_Move_InvalidPosition()
+        {
+            CommandProcessor commandProcessor = new CommandProcessor();
+            commandProcessor.XMax = 5;
+            commandProcessor.YMax = 5;
+            commandProcessor.XPosRobot = 4;
+            commandProcessor.YPosRobot = 4;
+            commandProcessor.FaceDirection = Direction.EAST;
+            commandProcessor.Move();
+            Assert.AreEqual("4,4,EAST", commandProcessor.XPosRobot + "," + commandProcessor.YPosRobot + "," + commandProcessor.FaceDirection);
+        }
+
+        [TestMethod]
+        public void CommandProcessor_Move_Success()
+        {
+            CommandProcessor commandProcessor = new CommandProcessor();
+            commandProcessor.XMax = 5;
+            commandProcessor.YMax = 5;
+            commandProcessor.XPosRobot = 3;
+            commandProcessor.YPosRobot = 4;
+            commandProcessor.FaceDirection = Direction.EAST;
+            commandProcessor.Move();
+            Assert.AreEqual("4,4,EAST", commandProcessor.XPosRobot + "," + commandProcessor.YPosRobot + "," + commandProcessor.FaceDirection);
         }
 
     }
